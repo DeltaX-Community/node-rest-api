3c843ecc127ebc9d7fd10ff38e8a9d2539eded9d
Damián Simañuk <damiansimanuk@gmail.com>
2021-08-12 16:16:50 -0300

    - improve user e2e test using swagger generated schema

--- src/e2e/Users/users.e2e.ts
+++ src/e2e/Users/users.e2e.ts
@@ -6,13 +6,16 @@ export function usersDescribe(app) {
   it("GET /api/v1/users", async () => {
     // Auth
     const resAuth = await request(app).post("/auth/login").send({ password: "", username: "admin" })
+    type IAuthGetResponse =
+      paths["/auth/login"]["post"]["responses"]["200"]["content"]["application/json"]
 
-    const accessToken = (resAuth.body as { accessToken: string }).accessToken
-    expect(accessToken).not.to.be.empty
+    const resultLogin = resAuth.body as IAuthGetResponse
+    expect(resultLogin.refreshToken).not.to.be.empty
+    expect(resultLogin.accessToken).not.to.be.empty
 
     const res = await request(app)
       .get("/api/v1/users")
-      .auth(accessToken, { type: "bearer" })
+      .auth(resultLogin.accessToken, { type: "bearer" })
       .query({ page: 2, perPage: 1 })
       .send()
     expect(res.status).to.equal(200)
@@ -27,6 +30,9 @@ export function usersDescribe(app) {
     expect(result.rows.length).to.eq(1)
     expect(result.rows[0].username).not.to.be.empty
     expect(result.rows[0].fullName).not.to.be.empty
+    expect(result.rows[0].updatedAt).not.to.be.empty
+    expect(result.rows[0].createAt).not.to.be.empty
+    expect(result.rows[0].isActive).to.eq(true)
     // Fixme: Test all Row fields
   })
 }
